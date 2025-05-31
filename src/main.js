2fbbb729dfe90d0031f20e15fcf33a8801230efd
OscarS5AN <odd6648@gmail.com>
2025-05-31 00:08:18 -0500

    Acomode el main.js que movi por error

--- src/main.js
+++ src/main.js
@@ -1,73 +1,24 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/HomeView.vue'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-//Pizza Sizes
-import EditPizza from '../components/pizzas/EditPizza.vue'
-import NewPizza from '../components/pizzas/NewPizza.vue'
-import PizzaSizes from '../views/PizzaSizes.vue'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBank, faBuilding, faPencil, faPhone, faPlus, faTag, faTrash, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { createApp } from 'vue';
 
-// Pizzas
-import EditPizzaSize from '../components/pizzasizes/EditPizzaSize.vue'
-import NewPizzaSize from '../components/pizzasizes/NewPizzaSize.vue'
-import Pizzas from '../views/Pizzas.vue'
+import App from './App.vue';
+import router from './router';
 
-//Ingredients
-import EditIngredient from '../components/ingredients/EditIngredient.vue'
-import NewIngredient from '../components/ingredients/NewIngredient.vue'
-import Ingredients from '../views/Ingredients.vue'
+library.add(faPhone);
+library.add(faUser);
+library.add(faTrash);
+library.add(faPencil);
+library.add(faTag);
+library.add(faBuilding);
+library.add(faBank);
+library.add(faPlus);
 
-const routes = [
-  
-  {
-    path: '/pizzas',
-    name: 'Pizzas',
-    component: Pizzas
-  },
-  {
-    path: '/edit-pizza/:id',
-    name: 'EditPizza',
-    component: EditPizza
-  },
-  {
-    path: '/add-pizza',
-    name: 'NewPizza',
-    component: NewPizza
-  },
-  {
-    path: '/pizza-sizes',
-    name: 'PizzaSizes',
-    component: PizzaSizes
-  },
-  {
-    path: '/edit-pizza-size/:id',
-    name: 'EditPizzaSize',
-    component: EditPizzaSize
-  },
-  {
-    path: '/add-pizza-size',
-    name: 'NewPizzaSize',
-    component: NewPizzaSize
-  },
-  {
-    path: '/ingredients',
-    name: 'Ingredients',
-    component: Ingredients
-  },
-  {
-    path: '/edit-ingredient/:id',
-    name: 'EditIngredient',
-    component: EditIngredient
-  },
-  {
-    path: '/add-ingredient',
-    name: 'NewIngredient',
-    component: NewIngredient
-  }
-]
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
-
-export default router
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(router)
+  .mount('#app');
